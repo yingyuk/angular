985f20b27398805d4374091fa90f471bd52a7e95
yukyuk <yukdev@163.com>
2016-09-07 17:03:32 +0800

    双向数据绑定 编辑

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,37 @@
 /*
-* @Author: yukyuk
-* @Date:   2016-09-07 00:12:44
-* @Last Modified by:   yukyuk
-* @Last Modified time: 2016-09-07 00:12:59
-*/
+ * @Author: yukyuk
+ * @Date:   2016-09-07 00:12:44
+ * @Last Modified by:   yukyuk
+ * @Last Modified time: 2016-09-07 16:50:56
+ */
 
 'use strict';
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,22 @@
 * @Author: yukyuk
 * @Date:   2016-09-07 00:14:49
 * @Last Modified by:   yukyuk
-* @Last Modified time: 2016-09-07 00:14:54
+* @Last Modified time: 2016-09-07 16:49:06
 */
 
 'use strict';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
-
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
